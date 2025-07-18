71c2e32039a87a4d512aaac7cebdbdd2f896846c
BHUVAN-HUB5 <bbhuvan358@gmail.com>
2025-07-18 19:32:18 +0530

    Add files via upload

---
+++ BTE with graph.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+def estimate(act_bt,a,e_0):
+    pred_bt=[e_0]
+    for i in range(1,len(act_bt)):
+        prev_pred=pred_bt[-1]
+        pred=a*act_bt[i-1]+(1-a)*prev_pred
+        pred_bt.append(pred)
+    return pred_bt
+
+act_bt=[]
+n=int(input("enter no. of processes: "))
+print("enter burst time of each process: ")
+for i in range(n):
+    bt=float(input(f"Process {i+1}: "))
+    act_bt.append(bt)
+
+e_0=float(input("enter the initial predicted burst time: "))
+
+a=float(input("enter smooothing factor (0<=a<=1): "))
+
+
+pred_bt=estimate(act_bt,a,e_0)
+
+print("\nProcess\tActual_BT Predicted_BT")
+print("-----------------------------------------")
+for i in range(n):
+    print("P",i,"\t",act_bt[i],"\t    ",pred_bt[i],(" (Previous Prediction)" if i==0 else ""))
+    
+
+plt.plot(range(1,n+1), act_bt, marker="o", label="Actual Burst Time")
+plt.plot(range(1,n+1), pred_bt, marker="x", label="Predicted Burst Times", linestyle=":")
+plt.xlabel("Process")
+plt.ylabel("Burst time")
+plt.grid(True)
+plt.legend()
+plt.show()
